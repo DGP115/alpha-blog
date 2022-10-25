c62cd86cf48ef0c7a39c10a99229277aa4947692
David Peloso <david.g.peloso@gmail.com>
2022-10-25 16:13:01 -0400

    Added Home & About pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+
+class PagesController < ApplicationController
+
+  def home
+    # Comment for rubocop
+  end
+
+  def about
+    #  Comment to satisft rubocop
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  #  The controller is called "pages" (actually pages.controller.rb) and the
+  #   action to be invoked is call home , so the pages controller needs a method called home
+  root 'pages#home'
+
+  #  Adding an About (static) page:
+  #   The get tells Rails what to do and the
+  #   to tells it where to go to find the controller action for the About page
+  get 'about', to: 'pages#about'
+
 end
