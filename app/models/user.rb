842acbf3e8d0d64a4ab78d2e79e4c75488143db7
David Peloso <david.g.peloso@gmail.com>
2022-11-18 13:36:54 -0500

    Add users table & User model

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Class User serves as the Rails model for entity "user" in this blog app
+class User < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_27_002547) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_18_171712) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
