a42c3fb946b3b9e9a670ef23899e430a383c75d3
David Peloso <david.g.peloso@gmail.com>
2022-11-19 17:03:13 -0500

    add password authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,12 @@
 
 # Class User serves as the Rails model for entity "user" in this blog app
 class User < ApplicationRecord
+  before_save { self.email_address = email_address.downcase }
   has_many :articles
   validates :username,  presence: true, uniqueness: { case_sensitive: false },
                         length: { minimum: 3, maximum: 25 }
   validates :email_address, presence: true, uniqueness: { case_sensitive: false },
                             email: { mode: :strict, require_fqdn: true },
                             length: { maximum: 105 }
+  has_secure_password
 end
