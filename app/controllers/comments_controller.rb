2964d7e1a650d1e486181b75d25a9c52aed608cb
David Peloso <david.g.peloso@gmail.com>
2022-12-01 10:29:07 -0500

    Beta Comment UI

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,17 +2,10 @@
 
 # This is the controller for the Comments resource
 class CommentsController < ApplicationController
-  def new
-    # Create a new comment object immediately upon entry of this method so that
-    # a valid object exists when the Show etc. pages are first entered.
-    # Otherwise, the error trapping done there will fail
-    # @comment = Comment.new
-  end
-
   def create
     #  Recall the id of the parent article is "known" by the nested routes for Comments,
     #  so params provides us with the parent article
-    @article = Article.find(params[:id])
+    @article = Article.find(params[:article_id])
     @comment = @article.comments.create(whitelist_comment_params)
     redirect_to article_path(@article)
   end
